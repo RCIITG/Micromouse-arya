378665423818851d38297b097d672158ada06c70
ARYA KUMAR <83552524+arya-som@users.noreply.github.com>
2022-01-17 14:35:15 +0530

    Add files via upload

---
+++ catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/arya/const/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/arya/const/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/arya/const/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/arya/const/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/arya/const/src:/home/arya/sim_ws/src:/opt/ros/melodic/share'
